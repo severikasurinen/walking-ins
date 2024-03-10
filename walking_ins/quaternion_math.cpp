4f2d28b8731cc1372d9bd29e4c2156ade66884ea
ArskaPihal <aarne.piha@aalto.fi>
2024-03-10 20:44:24 +0200

    Update quaternion_math.cpp

--- walking_ins/quaternion_math.cpp
+++ walking_ins/quaternion_math.cpp
@@ -1,4 +1,5 @@
 #include "quaternion_math.h"
+#include <cmath>
 
 Quaternion::Quaternion() {
   w = 1.0f;
@@ -67,3 +68,40 @@ Quaternion EulerToQuaternion(float roll, float pitch, float yaw)  // roll (x), p
 
   return q;
 }
+
+Quaternion OffsetQ(Vector sensorRead) // algorithm to generate the offset quaternion, where the vector sensorRead contains the linear accelerations returned by the sensor
+{
+  Vector g; //the normalized gravity vector
+  g.x = 0;
+  g.y = 0;
+  g.z = 1;
+
+  Vector c; //c for cross product
+  c.x = sensorRead.y * g.z - sensorRead.z * g.y
+  c.y = sensorRead.x * g.z - sensorRead.z * g.x
+  c.z = sensorRead.x * g.y - sensorRead.y * g.x
+
+  float norm_s = sensorRead.x * sensorRead.x + sensorRead.y * sensorRead.y + sensorRead.z * sensorRead.z
+  float norm_g = 1 //norm of the normalized g is one
+  float dot_sg = sensorRead.z //x and y components evaluate to zero
+
+  Quaternion offset; //the unnormalized local to global rotation quaternion
+  offset.w = sqrt(norm_s * norm_g) + dot_sg
+  offset.x = c.x
+  offset.y = c.y
+  offset.z = c.z
+
+  float norm_o = sqrt(offset.w * offset.w + offset.x * offset.x + offset.y * offset.y + offset.z * offset.z)
+
+  Quaternion normalized_offset;
+  normalized_offset.w = (1/norm_o)*offset.w
+  normalized_offset.x = (1/norm_o)*offset.x
+  normalized_offset.y = (1/norm_o)*offset.y
+  normalized_offset.z = (1/norm_o)*offset.z
+
+  return normalized_offset
+
+}
+
+
+
