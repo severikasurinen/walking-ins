04ba533d465ad4c6f3d9320dc57d59a295bb737a
ArskaPihal <aarne.piha@aalto.fi>
2024-04-11 16:07:41 +0300

    sending data stuff moved to .ino

--- walking_ins/walking_ins.ino
+++ walking_ins/walking_ins.ino
@@ -6,6 +6,9 @@
 
 #define SLEEP_TIME 600  // DO NOT SET BELOW 15 SECONDS!!!
 #define POWER_LED 8
+#define SEND_TIME 1000        // Time in ms between sending data via bluetooth
+
+uint32_t t_sent = 0;     // Since when was the data last sent to the UI?
 
 void setup() {
   delay(3000);
@@ -47,7 +50,22 @@ void loop() {
       if (device_moving) {
 
       } else {
-        SendData(millis());
+          if(t_sent > TIME_SEND) { //send data if enough time has elapsed
+    float send_data = send_data[8];
+    send_data[0] = (float) millis();
+    send_data[1] = position[0];
+    send_data[2] = position[1];
+    send_data[3] = position[2];
+    send_data[4] = rotation[3];
+    send_data[5] = rotation[4];
+    send_data[6] = rotation[5];
+    send_data[7] = rotation[6];
+    t_sent = 0;
+    size_t length = 8;
+    
+  } else {
+    t_sent = millis();
+  }
       }
       break;
     default:
