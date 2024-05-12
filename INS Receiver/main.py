67432e27ed8eed4b11f788c9926e46cd38ddb0ac
Severi Kasurinen <61545985+severikasurinen@users.noreply.github.com>
2024-05-12 12:32:40 +0300

    Automatic retry for connection

--- INS Receiver/main.py
+++ INS Receiver/main.py
@@ -108,44 +108,48 @@ class Window(tk.Tk):
         while True:
             timestamp = None
             if is_measuring:
-                in_data = await device_client.read_gatt_char(DATA_UUID)
-                if len(in_data) == 32:
-                    timestamp = struct.unpack('<L', in_data[0:4])[0]
-                    pos_x = struct.unpack('<f', in_data[4:8])[0]
-                    pos_y = struct.unpack('<f', in_data[8:12])[0]
-                    pos_z = struct.unpack('<f', in_data[12:16])[0]
-                    rot_w = struct.unpack('<f', in_data[16:20])[0]
-                    rot_x = struct.unpack('<f', in_data[20:24])[0]
-                    rot_y = struct.unpack('<f', in_data[24:28])[0]
-                    rot_z = struct.unpack('<f', in_data[28:32])[0]
+                try:
+                    in_data = await device_client.read_gatt_char(DATA_UUID)
+                    if len(in_data) == 32:
+                        timestamp = struct.unpack('<L', in_data[0:4])[0]
+                        pos_x = struct.unpack('<f', in_data[4:8])[0]
+                        pos_y = struct.unpack('<f', in_data[8:12])[0]
+                        pos_z = struct.unpack('<f', in_data[12:16])[0]
+                        rot_w = struct.unpack('<f', in_data[16:20])[0]
+                        rot_x = struct.unpack('<f', in_data[20:24])[0]
+                        rot_y = struct.unpack('<f', in_data[24:28])[0]
+                        rot_z = struct.unpack('<f', in_data[28:32])[0]
 
-                    if data_points[-1][0] != timestamp:
-                        print("Timestamp:", timestamp, "| X:", pos_x, ", Y:", pos_y, ", Z:", pos_z,
-                              "| W:", rot_w, ", X:", rot_x, ", Y:", rot_y, ", Z:", rot_z)
-                        data_points.append([timestamp, pos_x, pos_y, pos_z, rot_w, rot_x, rot_y, rot_z])
+                        if data_points[-1][0] != timestamp:
+                            print("Timestamp:", timestamp, "| X:", pos_x, ", Y:", pos_y, ", Z:", pos_z,
+                                  "| W:", rot_w, ", X:", rot_x, ", Y:", rot_y, ", Z:", rot_z)
+                            data_points.append([timestamp, pos_x, pos_y, pos_z, rot_w, rot_x, rot_y, rot_z])
 
-                        # Draw data points and lines
-                        if len(data_points) > 1:
-                            x = np.array(data_points)[:, 1]
-                            y = np.array(data_points)[:, 2]
+                            # Draw data points and lines
+                            if len(data_points) > 1:
+                                x = np.array(data_points)[:, 1]
+                                y = np.array(data_points)[:, 2]
 
-                            fig = plt.figure()
-                            fig.patch.set_alpha(0.0)
-                            ax = fig.add_subplot(111)
-                            plt.xlabel("x (m)")
-                            plt.ylabel("y (m)")
-                            plt.axis((-fig_range[0], fig_range[0], -fig_range[1], fig_range[1]))
-                            plt.grid()
+                                fig = plt.figure()
+                                fig.patch.set_alpha(0.0)
+                                ax = fig.add_subplot(111)
+                                plt.xlabel("x (m)")
+                                plt.ylabel("y (m)")
+                                plt.axis((-fig_range[0], fig_range[0], -fig_range[1], fig_range[1]))
+                                plt.grid()
 
-                            ax.plot(x, y, color='red')
+                                ax.plot(x, y, color='red')
 
-                            fig.savefig('temp.png', facecolor=fig.get_facecolor(), edgecolor='none')
-                            plt.close()
-                            img = ImageTk.PhotoImage(Image.open("temp.png"))
-                            os.remove("temp.png")
-                            self.canvas.create_image(fig_size[0] / 2, 0, anchor=tk.N, image=img)
+                                fig.savefig('temp.png', facecolor=fig.get_facecolor(), edgecolor='none')
+                                plt.close()
+                                img = ImageTk.PhotoImage(Image.open("temp.png"))
+                                os.remove("temp.png")
+                                self.canvas.create_image(fig_size[0] / 2, 0, anchor=tk.N, image=img)
 
-                    await asyncio.sleep(0.15)
+                        await asyncio.sleep(0.15)
+                except Exception as e:
+                    print(e)
+                    await self.toggle_connection()
 
             if timestamp is None:
                 self.label["text"] = timestamp
@@ -157,27 +161,43 @@ class Window(tk.Tk):
 
     async def toggle_connection(self):
         global device_connected, device_client
-        if device_connected:
-            if await disconnect(address):
-                device_connected = False
-                device_client = None
-                self.conn_button["text"] = 'Connect'
-                self.conn_button["bg"] = 'green'
-                self.control_button["state"] = DISABLED
-                self.control_button["bg"] = 'gray'
 
-        else:
-            if await connect(address):
-                device_connected = True
-                self.conn_button["text"] = 'Disconnect'
-                self.conn_button["bg"] = 'red'
-                self.control_button["state"] = NORMAL
-                self.control_button["bg"] = 'green'
+        self.conn_button["state"] = DISABLED
+
+        success = False
+        tries = 0
+        while not success and tries < 5:
+            if device_connected:
+                self.conn_button["text"] = 'Disconnecting...'
+                if await disconnect(address):
+                    device_connected = False
+                    device_client = None
+                    self.conn_button["text"] = 'Connect'
+                    self.conn_button["bg"] = 'green'
+                    self.control_button["state"] = DISABLED
+                    self.control_button["bg"] = 'gray'
+                    success = True
+
+            else:
+                self.conn_button["text"] = 'Connecting...'
+                if await connect(address):
+                    device_connected = True
+                    self.conn_button["text"] = 'Disconnect'
+                    self.conn_button["bg"] = 'red'
+                    self.control_button["state"] = NORMAL
+                    self.control_button["bg"] = 'green'
+                    success = True
+            tries += 1
+
+        self.conn_button["state"] = NORMAL
 
     async def toggle_measurement(self):
         global is_measuring, data_points
 
+        self.control_button["state"] = DISABLED
+
         if is_measuring:
+            self.control_button["text"] = 'Stopping...'
             if await stop_measuring():
                 is_measuring = False
                 self.control_button["text"] = 'Start'
@@ -194,6 +214,7 @@ class Window(tk.Tk):
                     csv_writer.writerow(['timestamp', 'pos_x', 'pos_y', 'pos_z', 'rot_w', 'rot_x', 'rot_y', 'rot_z'])
                     csv_writer.writerows(data_points)
         else:
+            self.control_button["text"] = 'Starting...'
             if await start_measuring():
                 self.canvas.delete("all")
                 data_points = [[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]]
@@ -202,6 +223,8 @@ class Window(tk.Tk):
                 self.control_button["bg"] = 'red'
                 self.conn_button["bg"] = 'gray'
                 self.conn_button["state"] = DISABLED
+
+        self.control_button["state"] = NORMAL
 
 
 def main():
